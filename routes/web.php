6a985a6f309aeac805ca43b0ae234792b3aafd23
2-Mohamed-2 <mohamed.ni.mohamed>
2022-12-03 23:04:07 +0000

    ok

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\laravel_example\UserManagement;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Essai
 
 $controller_path = 'App\Http\Controllers';
 
