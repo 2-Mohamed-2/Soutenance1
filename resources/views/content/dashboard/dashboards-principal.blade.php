bc7a1b057d477986987678aff771ad24b70215dd
Dev_madel <mohamed.2.intec@gmail.com>
2023-07-13 13:11:55 +0000

    ok

--- resources/views/content/dashboard/dashboards-principal.blade.php
+++ resources/views/content/dashboard/dashboards-principal.blade.php
@@ -17,6 +17,86 @@ $configData = Helper::appClasses();
 @section('page-script')
 <script src="{{asset('assets/js/dashboards-analytics.js')}}"></script>
 
+<script>
+  $(document).ready(function() {
+  $.ajax({
+  url: '/statistique',
+  dataType: 'json',
+  success: function(result) {
+  function getMois(i){
+  if(i == 1)
+  {
+  return "Janvier"
+  }
+  if(i == 2)
+  {
+  return "Fevrier"
+  }
+  if(i == 3)
+  {
+  return "Mars"
+  }
+  if(i == 4)
+  {
+  return "Avril"
+  }
+  if(i == 5)
+  {
+  return "Mai"
+  }
+  if(i == 6)
+  {
+  return "Juin"
+  }
+  if(i == 7)
+  {
+  return "Juillet"
+  }
+  if(i == 8)
+  {
+  return "Aout"
+  }
+  if(i == 9)
+  {
+  return "Septembre"
+  }
+  if(i == 10)
+  {
+  return "Octobre"
+  }
+  if(i == 11)
+  {
+  return "Novembre"
+  }
+  if(i == 12)
+  {
+  return "Decembre"
+  }
+
+  }
+  // Faites ce que vous voulez avec les données récupérées
+  console.log(result[0]);
+  const totalRevenueChartEl = document.querySelector('#totalRevenueChart');
+  const data = [];
+  const month = [];
+  const years = [];
+  // bonjour
+  for (var i = 0; i < result.length; i++) { data.push(result[i].count); month.push(getMois(result[i].mois));
+    years.push(result[i].annee); } const totalRevenueChartOptions={ series: [ { name: years, data: data } ], chart: {
+    height: 300, stacked: true, type: 'bar' , toolbar: { show: false } }, plotOptions: { bar: { horizontal: false,
+    columnWidth: '33%' , borderRadius: 12, startingShape: 'rounded' , endingShape: 'rounded' } }, colors:
+    [config.colors.primary, config.colors.info], dataLabels: { enabled: false }, stroke: { curve: 'smooth' , width: 6,
+    lineCap: 'round' , colors: [cardColor] }, legend: { show: true, horizontalAlign: 'left' , position: 'top' , markers: {
+    height: 8, width: 8, radius: 12, offsetX: -3 }, labels: { colors: legendColor }, itemMargin: { horizontal: 10 } },
+    grid: { borderColor: borderColor, padding: { top: 0, bottom: -8, left: 20, right: 20 } }, xaxis: { categories: month,
+    labels: { style: { fontSize: '13px' , colors: labelColor } }, axisTicks: { show: false }, axisBorder: { show: false }
+    }, yaxis: { labels: { style: { fontSize: '13px' , colors: labelColor } } }, responsive: [ // Définition des options
+    responsives ], states: { hover: { filter: { type: 'none' } }, active: { filter: { type: 'none' } } } }; if (typeof
+    totalRevenueChartEl !=='undefined' && totalRevenueChartEl !==null) { const totalRevenueChart=new
+    ApexCharts(totalRevenueChartEl, totalRevenueChartOptions); totalRevenueChart.render(); } }, error: function(error) {
+    console.error(error); } }); });
+</script>
+
 @endsection
 
 
